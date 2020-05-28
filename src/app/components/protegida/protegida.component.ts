2026954269c2f41b566bc521bb00d94f9c4db4bb
villasana26enrique <enrique.villasana26@gmail.com>
2020-05-27 22:11:41 -0400

    Mostrando Información del User en la opción protegida. El error de que se pierde la sesión al recargar la pantalla, se soluciona ingresando al panel de Auth0, Luego en Global Logins y se selecciona la configuración "new" esto para que utilice la nueva configuración del Auth0

--- src/app/components/protegida/protegida.component.ts
+++ src/app/components/protegida/protegida.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-protegida',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProtegidaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit(): void {
+    console.log('Protegida');
+    /* Se puede obtener la información del perfil a traves del observable "userProfile$" 
+    * su nombre tiene un '$' que sirve para indicarnos que es un observable, por eso utilizamos
+    * el subscribe. Pero esta vez no vamos a obtener la información del Perfil de esta manera,
+    * sino directamente en el HTML, por eso no almacenamos la información en ninguna variable 
+    * y tampoco la enviamos al HTML.
+    * */
+    this.auth.userProfile$.subscribe(profile => console.log(profile));
   }
 
 }
