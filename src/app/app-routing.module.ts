384ab17197bb5514be5241a9e01528fb3068ba7f
villasana26enrique <enrique.villasana26@gmail.com>
2020-05-27 21:50:37 -0400

    Protegiendo ruta con un Guard para que no se pueda acceder a ella si no esta logueado. Ocurre un error al refrescar la pagina no mantiene la sesión

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './services/auth.guard';
+
 import { HomeComponent } from './components/home/home.component'
 import { PreciosComponent } from './components/precios/precios.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 
 
+
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'precios', component: PreciosComponent},
-  {path: 'protegida', component: ProtegidaComponent},
+  {path: 'protegida', component: ProtegidaComponent, canActivate: [AuthGuard]},
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
